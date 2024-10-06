cde14a73e2ef4316e46530d1f02512cdf8da146e
Emma-Jane MacKinnon-Lee <emma.jane@digitalax.xyz>
2024-10-05 21:14:40 -0400

    init

---
+++ src/NPCVoting.sol
@@ -0,0 +1,11 @@
+// votando por los espectadores sobre los npcs y las publicaciones específicas 
+// su actividad en la tabla 
+// el aquiler entre los npcs + los espectadores
+// los npcs tienen que pagar el aquiler y el flujo del AU
+// los manuales más tarde por la activadad de los npcs :)
+
+// SPDX-License-Identifier: UNLICENSE
+
+pragma solidity ^0.8.26;
+
+contract NPCVoting { }
